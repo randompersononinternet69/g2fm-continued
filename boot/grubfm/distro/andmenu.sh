f24154ef038696ca8d502563d43d344f6eb00082
TCFFan123 <tcffan123@outlook.com>
2024-06-24 14:10:52 -0400

    add Install Android entry for Android x86

--- boot/grubfm/distro/andmenu.sh
+++ boot/grubfm/distro/andmenu.sh
@@ -4,23 +4,34 @@ menuentry $"Android-x86 Live" --class android{
     initrd $initrd_img;
 }
 if [ "${android_selinux}" = " " ]; then
-    menuentry "[ ] androidboot.selinux=permissive"{
+    menuentry "[ ] androidboot.selinux=permissive" --class settings{
         export android_selinux="androidboot.selinux=permissive";
         configfile ${prefix}/distro/andmenu.sh
     }
 else
-    menuentry "[+] androidboot.selinux=permissive"{
+    menuentry "[+] androidboot.selinux=permissive" --class settings{
         export android_selinux=" ";
         configfile ${prefix}/distro/andmenu.sh;
     }
 fi;
+if [ "${android_install}" = " " ]; then
+    menuentry "[ ] Install Android" --class settings{
+        export android_install="INSTALL=1";
+        configfile ${prefix}/distro/andmenu.sh
+    }
+else
+    menuentry "[+] Install Android" --class settings{
+        export android_install=;
+        configfile ${prefix}/distro/andmenu.sh;
+    }
+fi;
 if [ "${android_hardware}" = " " ]; then
-    menuentry "[ ] androidboot.hardware=android_x86"{
+    menuentry "[ ] androidboot.hardware=android_x86" --class settings{
         export android_hardware="androidboot.hardware=android_x86";
         configfile ${prefix}/distro/andmenu.sh
     }
 else
-    menuentry "[+] androidboot.hardware=android_x86"{
+    menuentry "[+] androidboot.hardware=android_x86" --class settings{
         export android_hardware=" ";
         configfile ${prefix}/distro/andmenu.sh;
     }
