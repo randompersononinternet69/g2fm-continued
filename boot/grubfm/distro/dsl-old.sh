341d908ec7fa58a07965523b94d4238eeb111161
TCFFan123 <tcffanyt+github@gmail.com>
2024-07-17 21:02:25 -0400

    Update dsl-old.sh

--- boot/grubfm/distro/dsl-old.sh
+++ boot/grubfm/distro/dsl-old.sh
@@ -1,13 +1,13 @@
 # none of these options will work under EFI, the kernel is too old
 menuentry "DSL"
-linux linux24 ramdisk_size=100000 init=/etc/init lang=us apm=power-off vga=791 nomce noapic quiet BOOT_IMAGE=knoppix
-initrd /boot/isolinux/minirt24.gz
+    linux linux24 ramdisk_size=100000 init=/etc/init lang=us apm=power-off vga=791 nomce noapic quiet BOOT_IMAGE=knoppix
+    initrd /boot/isolinux/minirt24.gz
 }
-menuentry "DSL debug"
-linux linux24 ramdisk_size=100000 init=/etc/init lang=us apm=power-off vga=791 nomce noapic debug BOOT_IMAGE=knoppix
-initrd /boot/isolinux/minirt24.gz
+menuentry "DSL Debug"
+    linux linux24 ramdisk_size=100000 init=/etc/init lang=us apm=power-off vga=791 nomce noapic debug BOOT_IMAGE=knoppix
+    initrd /boot/isolinux/minirt24.gz
 }
-menuentry "install DSL"
-linux linux24 ramdisk_size=100000 init=/etc/init lang=us apm=power-off vga=791 nomce noapic debug BOOT_IMAGE=knoppix _install_
-initrd /boot/isolinux/minirt24.gz
+menuentry "Install DSL"
+    linux linux24 ramdisk_size=100000 init=/etc/init lang=us apm=power-off vga=791 nomce noapic debug BOOT_IMAGE=knoppix _install_
+    initrd /boot/isolinux/minirt24.gz
 }
