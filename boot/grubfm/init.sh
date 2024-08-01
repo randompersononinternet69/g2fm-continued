8d3ba9b976a039d02ed176cd40b718abafd94bd6
a random person on the internet <tcffanyt+github@gmail.com>
2024-08-01 18:29:15 -0400

    Update init.sh

--- boot/grubfm/init.sh
+++ boot/grubfm/init.sh
@@ -35,7 +35,7 @@ export grub_secureboot=$"Not available"
 stat -r -q -s RAM
 export RAM
 set color_normal=yellow/black
-echo  "---------------------"
+echo "----------------------"
 echo "SYSTEM INFORMATION"
 echo "----------------------"
 if $MBR; then echo Legacy\\MBR\\CSM ; fi
