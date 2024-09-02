483a022dd10781d8d4d3bbd08f5eabe2b824cc89
random69420 <random69420@debian>
2024-09-02 13:24:33 -0400

    fix grub download

--- update_grub2.sh
+++ update_grub2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-wget https://github.com/a1ive/grub/releases/download/latest/grub2-latest.tar.gz -O grub2-latest.tar.gz
+wget https://github.com/TCFFan123/grub2/releases/download/latest/grub2-latest.tar.gz -O grub2-latest.tar.gz
 echo Extracting files...
 tar --overwrite -xf grub2-latest.tar.gz
