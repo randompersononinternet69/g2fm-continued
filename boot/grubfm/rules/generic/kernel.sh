f4fb1f64f60b2ce49766c8e1c684c4192eead0ae
a random person on the internet <tcffan123@outlook.com>
2024-09-12 19:17:02 -0400

    load initrd along with kernel (separate entry)

--- boot/grubfm/rules/generic/kernel.sh
+++ boot/grubfm/rules/generic/kernel.sh
@@ -10,6 +10,26 @@ then
   }
 fi;
 
+if file --is-x86-linux "${grubfm_file}";
+then
+  menuentry $"Boot Linux Kernel with initrd" --class gnu-linux {
+    initrd_list=("initrd.img-*" "initrd-*img")
+    for initrd in "${initrd_list[@]}"; do
+      if [ -f "${grubfm_file%/*}/${initrd}" ]; then
+        initrd_file="${grubfm_file%/*}/${initrd}"
+        break
+      fi
+    done
+
+    if [ "$grub_platform" = "efi" ];
+    then
+      linuxefi "${grubfm_file}" "${initrd_file}";
+    else
+      linux "${grubfm_file}" "${initrd_file}";
+    fi;
+  }
+fi;
+
 if file --is-x86-multiboot "${grubfm_file}";
 then
   menuentry $"Boot Multiboot Kernel" --class exe {
