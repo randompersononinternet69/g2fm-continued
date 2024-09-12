edfa126d47af6f3825a3806f170ab40bfae7944e
a random person on the internet <tcffan123@outlook.com>
2024-09-12 19:07:02 -0400

    Update themes.lua

--- boot/grubfm/themes.lua
+++ boot/grubfm/themes.lua
@@ -1,6 +1,11 @@
 #!lua
-dir = "(memdisk)/boot/grubfm/themes/"
+-- Get the first HDD device (assuming it's hd0)
+local dev = "hd0"
 
+-- Set the themes directory on the first HDD
+local themes_dir = "/" .. dev .. "/boot/grub/themes/"
+
+-- Function to enumerate files in a directory
 function enum_file (name)
   command = "export theme_std=(memdisk)/boot/grubfm/themes/" .. name .. "/theme.txt;" .. 
   "export theme_fm=(memdisk)/boot/grubfm/themes/" .. name .. "/fm.txt;" .. 
@@ -13,7 +18,26 @@ function enum_file (name)
   grub.add_icon_menu ("screen", command, name)
 end
 
-grub.clear_menu ()
-grub.enum_file (enum_file, dir)
+-- Function to scan for themes in a directory
+function scan_themes (dir)
+  -- Iterate through the directories in the given directory
+  for file in grub.utils.iter_dir(dir) do
+    -- Check if the file is a directory
+    if grub.utils.is_dir(file) then
+      -- Check if the directory contains a theme.txt file
+      if grub.file_exists(file .. "/theme.txt") then
+        -- Call the enum_file function to process the theme
+        enum_file(file:match("(.*)/"))
+      end
+    end
+  end
+end
 
+-- Clear the menu
+grub.clear_menu ()
+
+-- Scan for themes in the themes directory on the first HDD
+scan_themes(themes_dir)
+
+-- Source the global script
 grub.script ("source ${prefix}/global.sh")
