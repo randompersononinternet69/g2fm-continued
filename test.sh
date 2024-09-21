c7caa9d455390876869a3e99ca1a458437eb7125
a random person on the internet <tcffan123@outlook.com>
2024-09-20 20:26:37 -0400

    updated g2fm scripts and configuration files, added echo statements for debugging, removed ver.txt file, and modified build and release scripts.

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 echo Booting grubfm_multiarch.iso with 2GB RAM, and 4 cores.
-kvm -m 2048 -smp 4 -cpu host -cdrom grubfm.iso "$1" "$2" "$3" "$4" "$5"
+kvm -m 2048 -smp 4 -cdrom grubfm.iso $1 $2 $3 $4 $5
