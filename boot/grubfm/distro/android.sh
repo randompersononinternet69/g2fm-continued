f72cf97aa6e5bd7dd50defdf9aa272fcc9dc2049
a random person on the internet <tcffan123@outlook.com>
2024-12-08 15:57:13 -0500

    Update android.sh

--- boot/grubfm/distro/android.sh
+++ boot/grubfm/distro/android.sh
@@ -2,5 +2,12 @@ export vmlinuz_img="(loop)/kernel";
 export initrd_img="(loop)/initrd.img";
 export android_selinux="";
 export android_hardware=" ";
+export android_install=" ";
+export vulkan=" ";
+export setupwizard=" ";
+export hwaccel=" ";
+export acpi=" ";
+export autoinstall=" ";
+export autoupdate=" ";
 export android="(loop)"
 configfile ${prefix}/distro/andmenu.sh;
